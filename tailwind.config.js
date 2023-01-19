105bf50cc7f2902202622791112f452a8c5847db
amir <amirazizihosein@gmail.com>
2023-01-19 14:15:42 +0330

    add icon & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      lx: "1440px",
+    },
+    extend: {
+      colors: {
+        veryDarkMagenta: "hsl(300, 43%, 22%)",
+        softPink: "hsl(333, 80%, 67%)",
+        darkGrayishMagenta: "hsl(303, 10%, 53%)",
+        lightGrayishMagenta: "hsl(300, 24%, 96%)",
+      },
+    },
   },
   plugins: [],
-}
+};
